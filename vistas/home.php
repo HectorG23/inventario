57015187a07874c7c0efbfee644a7d56a6832cfd
HectorG23 <hdgc2006@gmail.com>
2024-11-12 14:07:02 -0500

    inventario realizado

---
+++ vistas/home.php
@@ -0,0 +1,18 @@
+<div class="container is-fluid">
+    <h1 class="title">Home</h1>
+    <h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido'];?> !</h2>
+</div>
+
+<nav class="pagination is-centered is-rounded" role="navigation" aria-label="pagination">
+
+
+
+<ul class="pagination-list">
+    <li><a href="#" class="pagination-link">1</a></li>
+    <li><span class="pagination-ellipsis">&hellip;</span></li>
+    <li><a href="#" class="pagination-link is-current">2</a></li>
+    <li><a href="#" class="pagination-link">3</a></li>
+    <li><span class="pagination-ellipsis">&hellip;</span></li>
+    <li><a href="#" class="pagination-link">3</a></li>
+</ul>
+
